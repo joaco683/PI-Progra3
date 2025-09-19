206de9b642dc498cf96e44ca9448e99cd4c91543
rockysasso <fernandezsassos@udesa.edu.ar>
2025-09-19 14:08:55 -0300

    Finalice la funcionalidad de Detalle: movi la lógica a componente DetallePelicula, agregue fetch con API Key correcta y limpie la screen

---
+++ src/components/DetallePelicula/DetallePelicula.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class Detalle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pelicula: null
+    };
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0fff56e4cd11a172a6a90b73b1f4c1f1&language=es-ES`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ pelicula: data });
+  })
+  .catch(error => console.log("Error al cargar detalle:", error));
+
+
+  }
+  render() {
+    if (this.state.pelicula === null) {
+      return <h3>Cargando...</h3>;
+    }
+
+    return (
+      <div>
+        <h1>{this.state.pelicula.title}</h1>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${this.state.pelicula.poster_path}`}
+          alt={this.state.pelicula.title}
+        />
+        <p><strong>Fecha de estreno:</strong> {this.state.pelicula.release_date}</p>
+        <p><strong>Rating:</strong> {this.state.pelicula.vote_average}</p>
+        <p><strong>Duración:</strong> {this.state.pelicula.runtime} minutos</p>
+        <p><strong>Sinopsis:</strong> {this.state.pelicula.overview}</p>
+        <p>
+          <strong>Géneros:</strong>{" "}
+          {this.state.pelicula.genres
+            ? this.state.pelicula.genres.map((g, idx) => (
+                <span key={idx}>{g.name} </span>
+              ))
+            : "No disponible"}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Detalle;
--- src/screens/Detalle/Detalle.js
+++ src/screens/Detalle/Detalle.js
@@ -1,47 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
+import DetallePelicula from "../../components/DetallePelicula/DetallePelicula";
 
-class Detalle extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pelicula: null
-    };
-  }
-
-  componentDidMount() {
-    const id = this.props.match.params.id;
-
-    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=TU_API_KEY&language=es-ES`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ pelicula: data });
-      })
-      .catch(error => console.log("Error al cargar detalle:", error));
-  }
-
-  render() {
-    if (this.state.pelicula === null) {
-      return <h3>Cargando...</h3>;
-    }
-
-    return (
-      <div>
-        <h1>{this.state.pelicula.title}</h1>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${this.state.pelicula.poster_path}`}
-          alt={this.state.pelicula.title}
-        />
-        <p><strong>Fecha de estreno:</strong> {this.state.pelicula.release_date}</p>
-        <p><strong>Rating:</strong> {this.state.pelicula.vote_average}</p>
-        <p><strong>Duración:</strong> {this.state.pelicula.runtime} minutos</p>
-        <p><strong>Sinopsis:</strong> {this.state.pelicula.overview}</p>
-        <p>
-          <strong>Géneros:</strong>{" "}
-          {this.state.pelicula.genres.map((g) => g.name).join(", ")}
-        </p>
-      </div>
-    );
-  }
+// Screen: solo envuelve al componente
+function Detalle(props) {
+  return <DetallePelicula {...props} />;
 }
 
 export default Detalle;
