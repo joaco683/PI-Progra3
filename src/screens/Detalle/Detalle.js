f81dad34b6a9951c320a05ebdd96bd5abf5bf971
rockysasso <fernandezsassos@udesa.edu.ar>
2025-09-19 13:03:25 -0300

    Capturo id de la URL y hago fetch del detalle desde la API

--- src/screens/Detalle/Detalle.js
+++ src/screens/Detalle/Detalle.js
@@ -8,6 +8,17 @@ class Detalle extends Component {
     };
   }
 
+  componentDidMount() {
+  
+    const id = this.props.match.params.id;
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=TU_API_KEY&language=es-ES`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ pelicula: data });
+      })
+      .catch(error => console.log("Error al cargar detalle:", error));
+  }
+
   render() {
     return (
       <div>
@@ -18,6 +29,3 @@ class Detalle extends Component {
 }
 
 export default Detalle;
-
-
-
