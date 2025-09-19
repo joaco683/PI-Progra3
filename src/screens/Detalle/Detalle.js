5de821071f99ba6014f56ceae06ef02d6fde0869
rockysasso <fernandezsassos@udesa.edu.ar>
2025-09-19 13:06:31 -0300

    Renderizo detalle de la película con datos de la API

--- src/screens/Detalle/Detalle.js
+++ src/screens/Detalle/Detalle.js
@@ -9,8 +9,8 @@ class Detalle extends Component {
   }
 
   componentDidMount() {
-  
     const id = this.props.match.params.id;
+
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=TU_API_KEY&language=es-ES`)
       .then(response => response.json())
       .then(data => {
@@ -20,9 +20,25 @@ class Detalle extends Component {
   }
 
   render() {
+    if (this.state.pelicula === null) {
+      return <h3>Cargando...</h3>;
+    }
+
     return (
       <div>
-        <h1>Detalle</h1>
+        <h1>{this.state.pelicula.title}</h1>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${this.state.pelicula.poster_path}`}
+          alt={this.state.pelicula.title}
+        />
+        <p><strong>Fecha de estreno:</strong> {this.state.pelicula.release_date}</p>
+        <p><strong>Rating:</strong> {this.state.pelicula.vote_average}</p>
+        <p><strong>Duración:</strong> {this.state.pelicula.runtime} minutos</p>
+        <p><strong>Sinopsis:</strong> {this.state.pelicula.overview}</p>
+        <p>
+          <strong>Géneros:</strong>{" "}
+          {this.state.pelicula.genres.map((g) => g.name).join(", ")}
+        </p>
       </div>
     );
   }
