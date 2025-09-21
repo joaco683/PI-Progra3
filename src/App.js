4b50c1cc52110d3076217f9fc6a4d7c521ecac1a
rockysasso <fernandezsassos@udesa.edu.ar>
2025-09-21 13:43:04 -0300

    hago que el footer quede abajo para todas las paginas

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-
-import {Route, Switch} from "react-router-dom"
-import Home from "./screens/Home/Home"
-import Peliculas from "./screens/Peliculas/Peliculas"
-import Cartelera from "./screens/Cartelera/Cartelera"
-import Detalle from "./screens/Detalle/Detalle"
-import Favoritos from "./screens/Favoritos/Favoritos"
-import Busqueda from "./screens/Busqueda/Busqueda"
-import SearchResults from "./screens/SearchResults/SearchResults"
-import NotFound from "./screens/NotFound/NotFound"
-import Header from "./components/Header/Header"
-import Footer from "./components/Footer/Footer"
+import { Route, Switch } from "react-router-dom";
+import Home from "./screens/Home/Home";
+import Peliculas from "./screens/Peliculas/Peliculas";
+import Cartelera from "./screens/Cartelera/Cartelera";
+import Detalle from "./screens/Detalle/Detalle";
+import Favoritos from "./screens/Favoritos/Favoritos";
+import Busqueda from "./screens/Busqueda/Busqueda";
+import SearchResults from "./screens/SearchResults/SearchResults";
+import NotFound from "./screens/NotFound/NotFound";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Header />
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/populares" component={Peliculas} />
-        <Route path="/cartelera" component={Cartelera} />
-        <Route path="/detalle/id/:id" component={Detalle} />
-        <Route path="/favoritos" component={Favoritos} />
-        <Route path="/busqueda/:busqueda" component={Busqueda} />
-        <Route path="/search/:query" component={SearchResults} />
-        <Route component={NotFound} />
-      </Switch>
+      <main className="main-content">
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/populares" component={Peliculas} />
+          <Route path="/cartelera" component={Cartelera} />
+          <Route path="/detalle/id/:id" component={Detalle} />
+          <Route path="/favoritos" component={Favoritos} />
+          <Route path="/busqueda/:busqueda" component={Busqueda} />
+          <Route path="/search/:query" component={SearchResults} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
       <Footer />
     </div>
   );
 }
+
 
 export default App;
